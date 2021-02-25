4cb34dee3f1f3fb13b5878cf1ad779654274e58f
Paul Grimes <pgrimes@cfa.harvard.edu>
2021-02-25 17:52:52 -0500

    Got ScanData object to pass tests.

--- python_beamscanner/tests/test_scandata.py
+++ python_beamscanner/tests/test_scandata.py
@@ -9,13 +9,13 @@ Relies on the example beamscanner data file in `data/scan_data.csv`
 
 
 from pathlib import Path
-from typing import Dict, File
 
 import numpy as np
 
 import pytest
 
 from python_beamscanner.nearfield import ScanData
+
 
 @pytest.fixture
 def data_dir() -> Path:
@@ -26,11 +26,20 @@ def data_dir() -> Path:
 # expose them. If we had multiple tests that wanted to use the contents of this file,
 # we could simply add "loaded_example_values" as a parameter for each test.
 @pytest.fixture
-def example_data(data_dir) -> File:
-    with open(data_dir / "example_scan.csv", "r") as read_in:
-        return read_in
+def example_filename(data_dir):
+    return data_dir / "wSMA240_88-13mm_240GHz.csv"
 
 
+@pytest.mark.parametrize(
+    "min_x, max_x, n_x, window_len",
+    [
+        # (min_x, max_x, n_x, window_len)
+        (5, 10, 20, 5),
+        (5, 10, 31, 5),
+        (10, 40, 40, 10),
+        (0, 1, 81, 20),
+    ],
+)
 def test_kaiser_smooth(min_x, max_x, n_x, window_len):
     """Dumb test of kaiser smoothing an array of data"""
     # generate some data
@@ -39,3 +48,21 @@ def test_kaiser_smooth(min_x, max_x, n_x, window_len):
 
     # just check the lengths, as we assume numpy works!
     assert len(b) == len(a)
+
+
+def test_reading_data(example_filename):
+    """Test basic reading of data"""
+    d = ScanData()
+    d.load_csv_data(example_filename)
+
+    assert d.s21.shape == d.cal_data.shape
+
+    assert d.s21.shape[0] == len(d.x_values)
+    assert d.s21.shape[1] == len(d.y_values)
+
+    assert d.x_values[1] - d.x_values[0] == d.x_step
+    assert d.y_values[1] - d.y_values[0] == d.y_step
+    assert d.x_values[0] == d.x_limits[0]
+    assert d.x_values[-1] == d.x_limits[1]
+    assert d.y_values[0] == d.y_limits[0]
+    assert d.y_values[-1] == d.y_limits[1]
--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ dev_requirements = [
 ]
 
 requirements = [
-    numpy,
-    scipy,
-    matplotlib
+    "numpy",
+    "scipy",
+    "matplotlib"
 ]
 
 extra_requirements = {
@@ -90,3 +90,43 @@ setup(
     version="0.0.0",
     zip_safe=False,
 )
+
+py~=1.10.0
+colorama~=0.4.4
+numpy~=1.20.1
+pip~=21.0.1
+wheel~=0.36.2
+openssl~=1.1.1j
+keyring~=22.0.1
+attrs~=20.3.0
+toml~=0.10.2
+regex~=2020.11.13
+click~=7.1.2
+appdirs~=1.4.4
+pathspec~=0.8.1
+black~=20.8b1
+pytest~=6.2.2
+setuptools~=49.6.0
+pytz~=2021.1
+scipy~=1.6.1
+pkginfo~=1.7.0
+flake8~=3.8.4
+pyflakes~=2.2.0
+wcwidth~=0.2.5
+pluggy~=0.13.1
+iniconfig~=1.1.1
+atomicwrites~=1.4.0
+certifi~=2020.12.5
+chardet~=4.0.0
+requests~=2.25.1
+codecov~=2.1.11
+urllib3~=1.26.3
+six~=1.15.0
+coverage~=5.4
+idna~=2.10
+pyparsing~=2.4.7
+ipython~=7.20.0
+sphinx~=3.5.1
+docutils~=0.16
+jinja2~=2.11.3
+pycodestyle~=2.6.0
